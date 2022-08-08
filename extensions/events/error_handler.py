b22dd1adc3ee21ba776bb05d304b67176f0c1e2a
TheFarGG <ps4rtnite1234@gmail.com>
2022-08-08 18:46:24 +0530

    Fix error handler

--- extensions/events/error_handler.py
+++ extensions/events/error_handler.py
@@ -8,10 +8,10 @@ from utils.utils import Embed
 
 
 class ErrorHandler(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
+        bot.tree.error(self.app_command_error)
 
-    @app_commands.CommandTree.error()
     async def app_command_error(
         self, ctx: discord.Interaction, error: app_commands.AppCommandError
     ):
