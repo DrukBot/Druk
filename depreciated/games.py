1c5a7635154cd686c25703ab1e4222550aa10e5c
c0ld <dimrijaip@gmail.com>
2022-08-09 22:07:12 +0530

    Moved games cog to depreciated

--- depreciated/games.py
+++ depreciated/games.py
@@ -1,7 +1,7 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
-from components.games import AkinatorView
+from depreciated.gamesComponents import AkinatorView
 
 from utils.utils import Embed
 
