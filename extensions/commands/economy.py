54504378166cda392a2c38056e77479905298c9e
lukewain <lukewainhouse@outlook.com>
2022-08-17 18:10:18 +0100

    Stopped bots from being able to have accounts

--- extensions/commands/economy.py
+++ extensions/commands/economy.py
@@ -22,7 +22,7 @@ class Economy(commands.Cog):
         acc = await self.db.fetch('accounts', f"user_id = {user.id}")
         if not acc:
             await self.db.insert('accounts', (user.id, 100, 0))
-            acc = {'user_id': user.id, 'coins': 100, 'bank': 0}
+            acc = {'user_id': user.id, 'coins': 100, 'cash': 0}
         return acc
 
     @app_commands.command(name='work')
@@ -48,7 +48,10 @@ class Economy(commands.Cog):
         user: typing.Optional[discord.User] = None,
     ):
         if user is None:
-            user = ctx.user            
+            user = ctx.user
+        if user.bot:
+            await ctx.response.send_message(embed=utils.Embed.ERROR("Woah There", "<@{}> is a bot, you can't do that".format(user.id)))
+            return     
         acc = await self.fetch_or_create_account(user)
         coins, cash = acc['coins'], acc['cash']
         if user is not None:
@@ -101,6 +104,40 @@ class Economy(commands.Cog):
         await ctx.response.send_message(embed=success_embed)
 
 
+    @app_commands.command(name="rob")
+    @app_commands.describe(user="The user you want to rob")
+    @app_commands.checks.cooldown(1, 300)
+    async def rob(
+        self,
+        ctx: discord.Interaction,
+        user: discord.User
+    ):
+        if user.id == ctx.user.id:
+            await ctx.response.send_message(embed=utils.Embed.ERROR("Whoops", "You can't rob yourself!"))
+            return
+            
+        author_bal = await self.fetch_or_create_account(ctx.user)
+        victim_bal = await self.fetch_or_create_account(user)
+
+        if victim_bal['coins'] < 200:
+            await ctx.response.send_message(embed=utils.Embed.ERROR("Whoops", f"<@{user.id}> doesn't have enough coins to steal"))
+            return
+
+        luck = random.randint(65, 75)
+        r_amt = random.randint(110, 500)
+
+        if luck in (69, 70):
+            await self.db.update('accounts', {'coins': victim_bal['coins']-r_amt}, f'user_id={user.id}')
+            await self.db.update('accounts', {'coins': author_bal['coins']+r_amt}, f'user_id={ctx.user.id}')
+
+            await ctx.response.send_message(embed=utils.Embed.SUCCESS(f"You robbed <@{user.id}>", f"You stole {r_amt} coins from <@{user.id}>"))
+
+        else:
+            await self.db.update('accounts', {'coins': author_bal['coins']-r_amt}, f'user_id={ctx.user.id}')
+            
+            await ctx.response.send_message(embed=utils.Embed.ERROR(f"You tried to rob <@{user.id}>", f"You got caught robbing <@{user.id}> and were fined {r_amt} coins"))
+
+
 accounts_table = utils.Table(
     "accounts",
     [
@@ -109,6 +146,16 @@ accounts_table = utils.Table(
         utils.Column("cash", int),
     ],
     primary_key="user_id",
+)
+
+settings_table = utils.Table(
+    "settings",
+    [
+        utils.Column("user_id", int),
+        utils.Column("theft_pings", bool),
+        utils.Column("passive_mode", bool)
+    ],
+    primary_key="user_id"
 )
 
 async def setup(bot):
