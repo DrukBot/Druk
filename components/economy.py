aaa53cac4f6b58365a9b5fe3d9798ae36b847eb4
lukewain <lukewainhouse@outlook.com>
2022-08-21 10:48:26 +0100

    Fixed register view

--- components/economy.py
+++ components/economy.py
@@ -9,7 +9,7 @@ class RegisterUser(discord.ui.View):
         self.db = db
 
     @discord.ui.button(label="I Accept the rule!", style=discord.ButtonStyle.green)
-    async def callback(self, ctx: discord.Interaction, button: discord.ui.button):
+    async def callback(self, ctx: discord.Interaction, button: discord.ui.Button):
         acc = await self.db.fetch('accounts', f"user_id = {self.user.id}")
         if acc:
             await ctx.response.send_message("You are already a registered user!", ephemeral=True)
@@ -18,8 +18,8 @@ class RegisterUser(discord.ui.View):
         await self.db.insert('accounts', (self.user.id, 500, 0))
         await self.db.insert('settings', (self.user.id, True, False))
         embed = utils.Embed(description="Successfully registered your account!").set_author(name="User Registered!", icon_url=self.user.display_avatar.url)
-        self.clear_items()
-        await ctx.response.edit_message(embed=embed, ephemeral=True, view=self)
+        button.disabled = True
+        await ctx.response.edit_message(embed=embed, view=None)
 
     async def interaction_check(self, ctx: discord.Interaction) -> bool:
         if ctx.user.id != self.user.id:
